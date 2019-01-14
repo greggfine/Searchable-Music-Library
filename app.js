8099af79fdf044304bc15fba04b1e1323d09fa84
Gregg Fine <bbeez8500@gmail.com>
2019-01-14 14:24:48 -0500

    disable passport for portfolio

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express 				= require('express'),
 	  app					= express(),
 	//   mongoose 			    = require('mongoose'),
-	  passport 				= require('passport'),
+	//   passport 				= require('passport'),
 	  User 					= require('./models/user'),
-	  LocalStrategy 		= require('passport-local'),
+	//   LocalStrategy 		= require('passport-local'),
 	  methodOverride 		= require('method-override'),
 	  helmet				= require('helmet');
 
@@ -19,54 +19,13 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-// ====================================
-//  	PASSPORT AUTH
-// ====================================
-app.use(require('express-session')({
-	secret: 'Rusty is the best and cutest dog in the world',
-	resave: false,
-	saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// ==================
-//  	HOME/LOGIN
-// ==================
-
-//  LOGIN ROUTES
-//  render login form
 app.get('/', (req, res) => {
-	res.render('home');
+	res.redirect('/search');
 });
-
-// LOGIN Logic
-// Middleware
-app.post('/', passport.authenticate('local', {
-    successRedirect: '/search', 
-    failureRedirect: '/'
-}) ,function(req, res){
-});
-
-app.get('/logout', (req, res) => {
-	req.logout();
-	res.redirect('/');
-});
-
-function isLoggedIn(req, res, next) {
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect('/');
-}
 
 app.use("/register", register);
 
-app.use(isLoggedIn);
+// app.use(isLoggedIn);
 
 
 app.use("/files", files);
