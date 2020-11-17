3432b89fbdeaf3bdd5da7775a82d60befeaf7022
Gregg Fine <bbeez8500@gmail.com>
2020-11-17 17:32:11 -0500

    added static ip

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const express 				= require('express'),
 	  search   				= require('./routes/search'),
 	  files	    			= require('./routes/files');
 // app.use(helmet());
+
+var http, options, proxy, url;
+
+http = require("http");
+
+url = require("url");
+
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target  = url.parse("http://ip.quotaguard.com/");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.set('view engine', 'ejs');
