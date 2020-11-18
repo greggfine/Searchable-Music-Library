8333ecd7781e2adae5f60e24c5f10541990fc07d
Gregg Fine <bbeez8500@gmail.com>
2020-11-18 09:56:46 -0500

    a

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express 				= require('express'),
 	  app					= express(),
+	  helmet				= require('helmet');
       search   				= require('./routes/search'),
-	  helmet				= require('helmet'),
-	  files	    			= require('./routes/files');
-        app.use(helmet());
-
+	//   files	    			= require('./routes/files');
+      app.use(helmet());
+ 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.use("/files", files);
+// app.use("/files", files);
 app.use("/search", search);
 
 app.get('/', (req, res) => {
--- routes/search.js
+++ routes/search.js
@@ -1,63 +1,57 @@
 const express = require('express'),
-      mongoose = require('mongoose'),
-      Grid = require('gridfs-stream'),
+    //   mongoose = require('mongoose'),
+    //   Grid = require('gridfs-stream'),
       router  = express.Router();
 
-const mongoURI = process.env.MONGO_URI;
-mongoose.connect(mongoURI, {useUnifiedTopology: false});
-// mongoose.connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//     useUnifiedTopology: false
-//   });
+// const mongoURI = process.env.MONGO_URI;
+// mongoose.connect(mongoURI, {useUnifiedTopology: false});
 
 
-const conn = mongoose.createConnection(mongoURI);
+// const conn = mongoose.createConnection(mongoURI);
 
 // Init gfs
-var gfs;
+// var gfs;
 
-conn.once('open', () => {
-    // Init stream
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-})
+// conn.once('open', () => {
+//     gfs = Grid(conn.db, mongoose.mongo);
+//     gfs.collection('uploads');
+// })
 
 router.get('/', (req, res) => {
-    var genre, genre2, genre3, length, bpm;
-    genre = genre2 = genre3 = req.query.genre;
-    length = req.query.length,
-    bpm = req.query.bpm;
+        res.render("temp");
+    // var genre, genre2, genre3, length, bpm;
+    // genre = genre2 = genre3 = req.query.genre;
+    // length = req.query.length,
+    // bpm = req.query.bpm;
 
-    if (!genre && !bpm && !length) {
-        gfs.files.find({'metadata.genre': 'rock'})
-                 .toArray((err, files) => {
-                    res.render('search', { files, genre, bpm, length });
-                 })
-    } else if (genre) {
-        gfs.files.find({
-                $or: [
-                        { 'metadata.genre': genre },
-                        { 'metadata.genre2': genre2 },
-                        { 'metadata.genre3': genre3 }
-                    ]
-            })
-            .toArray((err, files) => {
-                res.render('search', { files, genre, bpm, length });
-            })
-    } else if (length) {
-        gfs.files.find({ 'metadata.length': length })
-            .toArray((err, files) => {
-                res.render('search', { files, genre, bpm, length });
-            })
-    }
-    else {
-        gfs.files.find({'metadata.bpm': bpm })
-            .toArray((err, files) => {
-                res.render('search', { files, genre, bpm, length });
-            })
-    }
+    // if (!genre && !bpm && !length) {
+    //     gfs.files.find({'metadata.genre': 'rock'})
+    //              .toArray((err, files) => {
+    //                 res.render('search', { files, genre, bpm, length });
+    //              })
+    // } else if (genre) {
+    //     gfs.files.find({
+    //             $or: [
+    //                     { 'metadata.genre': genre },
+    //                     { 'metadata.genre2': genre2 },
+    //                     { 'metadata.genre3': genre3 }
+    //                 ]
+    //         })
+    //         .toArray((err, files) => {
+    //             res.render('search', { files, genre, bpm, length });
+    //         })
+    // } else if (length) {
+    //     gfs.files.find({ 'metadata.length': length })
+    //         .toArray((err, files) => {
+    //             res.render('search', { files, genre, bpm, length });
+    //         })
+    // }
+    // else {
+    //     gfs.files.find({'metadata.bpm': bpm })
+    //         .toArray((err, files) => {
+    //             res.render('search', { files, genre, bpm, length });
+    //         })
+    // }
 })
 
 module.exports = router;
