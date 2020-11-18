3b4b26703203571c9c1be6f1b4e1fc3b412d5422
Gregg Fine <bbeez8500@gmail.com>
2020-11-18 07:41:40 -0500

    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express 				= require('express'),
-	  app					= express();
+	  app					= express(),
+      search   				= require('./routes/search');
 	//   helmet				= require('helmet'),
-	//   search   				= require('./routes/search'),
 	//   files	    			= require('./routes/files');
 // app.use(helmet());
 
@@ -32,13 +32,14 @@ app.use(express.json())
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// app.use("/files", files);
+app.use("/search", search);
+
 app.get('/', (req, res) => {
-    // res.redirect('/search');
-    res.send("HT Music Library")
+    res.redirect('/search');
 });
 
-// app.use("/files", files);
-// app.use("/search", search);
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Our app is running on http://localhost:${port}`));
